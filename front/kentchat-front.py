f2107ddb63a6f83dbe9b0fba7faf817c44de449b
jeub <jurgen.palsma@gmail.com>
2017-11-21 14:18:14 +0000

    add cookies and sessions. Connect auth and login to api via placeholder values

--- front/kentchat-front.py
+++ front/kentchat-front.py
@@ -4,12 +4,16 @@ import urllib
 
 import jinja2
 import webapp2
+from webapp2_extras import sessions # for cookies
+
+
 from google.appengine.api import users
 from google.appengine.ext import ndb
 
 import random
 import requests
 import requests_toolbelt.adapters.appengine
+import json
 
 # Use the App Engine Requests adapter. This makes sure that Requests uses
 # URLFetch.
@@ -21,41 +25,76 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-class MainPage(webapp2.RequestHandler):  # Handler for GET '/'
+
+class BaseHandler(webapp2.RequestHandler):              # taken from the webapp2 extra session example
+    def dispatch(self):                                 # override dispatch
+        # Get a session store for this request.
+        self.session_store = sessions.get_store(request=self.request)
+
+        try:
+            # Dispatch the request.
+            webapp2.RequestHandler.dispatch(self)       # dispatch the main handler
+        finally:
+            # Save all sessions.
+            self.session_store.save_sessions(self.response)
+
+    @webapp2.cached_property
+    def session(self):
+        # Returns a session using the default cookie key.
+        return self.session_store.get_session()
+
+
+class MainPage(BaseHandler):
 
     def get(self):
+        print("GET /")
         template_values = {
             'user': "john",
+            'foo': self.session.get('user-key')
         }
         template = JINJA_ENVIRONMENT.get_template('index.html')
-        self.response.write(template.render(template_values))
+        #self.response.write(template.render(template_values))
+        self.response.write("hi")
 
 
-
-class LoginController(webapp2.RequestHandler):  # Handler for GET '/'
+class LoginController(BaseHandler):
 
     def get(self):
-        print("POST /login")
+        print("GET /login")
+
+        url = API_URL + "/auth"
+        data = dict(name='joe', email='joe@kent.ac.uk', password="pass")
+
+        response = requests.post(url, data=data, allow_redirects=True)
+        data = json.loads(response.content)
+        self.session['user-token'] = data['token']
+
+        self.response.write(self.session.get('user-token'))
+
+
+class RegistrationController(BaseHandler):
+
+    def get(self):
+        print("GET /register")
 
         url = API_URL + "/users"
         data = dict(name='joe', email='joe@kent.ac.uk', password="pass")
 
         response = requests.post(url, data=data, allow_redirects=True)
-        self.response.write(response.text)
+        data = json.loads(response.content)
+        self.session['user-key'] = data['key']
 
-    def post(self):
-        print("POST /login")
-
-        url = API_URL + "/users"
-        data = dict(name='joe', email='joe@kent.ac.uk', password="pass")
-
-        response = requests.post(url, data=data, allow_redirects=True)
-        self.response.write(response.read())
+        self.response.write(self.session.get('user-key'))
 
 
-
+config = {}
+config['webapp2_extras.sessions'] = {
+    'secret_key': 'my_super_secret_key',
+}
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/login', LoginController),
-], debug=True)
+    ('/register', RegistrationController)
+], config=config,
+    debug=True)
